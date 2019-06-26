84169bc08a0fcda592ff9c3d67c042688c4ba27d
doraneko94 <shuntaro94@gmail.com>
2019-06-26 13:56:47 +0900

    20190625

--- tensorflow/13_3.py
+++ tensorflow/13_3.py
@@ -13,10 +13,15 @@ filters[:, 3, :, 0] = 1
 filters[3, :, :, 1] = 1
 
 X = tf.placeholder(tf.float32, shape=(None, height, width, channels))
-convolution = tf.nn.conv2d(X, filters, strides=[1, 2, 2, 1], padding="SAME")
+# convolution = tf.nn.conv2d(X, filters, strides=[1, 2, 2, 1], padding="SAME")
+convolution = tf.layers.conv2d(X, filters=2, kernel_size=7, strides=[2, 2], padding="SAME")
+init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
+    init.run()
     output = sess.run(convolution, feed_dict={X: dataset})
 
+print(channels)
+print(output.shape)
 plt.imshow(output[0, :, :, 1], cmap="gray")
 plt.show()
